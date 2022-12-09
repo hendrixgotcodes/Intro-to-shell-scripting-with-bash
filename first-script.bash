c69c3a1d0204d34e910cdf5b10f9642303cc899f
Samuel O. Asare <asare11samuel@gmail.com>
2022-12-09 16:52:41 +0000

    feat: variables

---
+++ first-script.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#cookbook filename: variable naming and referencing
+
+MYVAR="something"
+echo $MYVAR
+
+MY_2ND_VAR=anotherone
+echo $MY_2ND_VAR
+
+MY_3RD_VAR="ok we are done"
+echo $MY_3RD_VAR
