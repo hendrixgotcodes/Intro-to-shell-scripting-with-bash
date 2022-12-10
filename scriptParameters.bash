435778e3f4113922e40bbd01e63a4754f32c74d6
Samuel O. Asare <asare11samuel@gmail.com>
2022-12-10 18:12:18 +0000

    feat: script parameters and arguments

--- scriptParameters.bash
+++ scriptParameters.bash
@@ -6,3 +6,7 @@
 echo "You supplied the following as argument: ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10}"
 #input: hello hello listen bbc news, on friday night
 #output: You supplied the following as argument: hello hello listen bbc news, on friday night 
+
+# You can use * to catch all arguments passed to the script
+echo "You can also do this ${*}"
+
