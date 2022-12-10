f3f79f684c9dd83f6230e61d8913ad50d2a91750
Samuel O. Asare <asare11samuel@gmail.com>
2022-12-10 21:18:47 +0000

    feat: handling parameters with spaces

--- scriptParameters.bash
+++ scriptParameters.bash
@@ -3,14 +3,20 @@
 # available to the script as numbered variables
 
 #example this will printout the first word that comes after this script when called
-echo "You supplied the following as argument: ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10}"
+### echo "You supplied the following as argument: ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10}"
 #input: hello hello listen bbc news, on friday night
 #output: You supplied the following as argument: hello hello listen bbc news, on friday night 
 
 # You can use * to catch all arguments passed to the script
-echo "You can also do this ${*}"
+# echo "You can also do this ${*}"
 
-for FN in $*
+### for FN in $*
+### do
+###     echo argument ${FN}
+### done
+
+# To handle list of parameters with spaces use "$@" instead of "$*"
+for FN in "$@"
 do
-    echo argument ${FN}
+    echo $FN
 done
